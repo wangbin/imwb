ca44a48d0f168d449f169bb3dcdd7be53919658e
Wang Bin <wangbin.zibo@gmail.com>
2013-11-26 13:37:33 +0800

    added RandomString function

---
+++ utils/crypto.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+const (
+	AllowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+func RandomString() string {
+	result := make([]byte, 12)
+	length := len(AllowedChars)
+	for index := range result {
+		result[index] = AllowedChars[rand.Intn(length)]
+	}
+	return string(result)
+}
