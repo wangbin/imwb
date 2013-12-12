70f4032dc62e64b832c1d381999c7d4b4614c31b
Wang Bin <wangbin.zibo@gmail.com>
2013-12-12 18:24:19 +0800

    sadly the current version of rethinkdb golang driver not implement any date related function, think about switch to another one

--- controllers/blog.go
+++ controllers/blog.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	r "github.com/christopherhesse/rethinkgo"
 	"github.com/wangbin/imwb/models/blog"
 	"github.com/wangbin/imwb/settings"
+	"time"
 )
 
 const (
@@ -31,11 +33,26 @@ func (this *PostListController) Finish() {
 func (this *PostListController) Get() {
 	this.TplNames = "post-list.html"
 	var posts []*blog.Post
+	query := r.Table(blog.PostTable)
 	page, err := this.GetInt("page")
 	if err != nil || page < 0 {
 		page = 0
 	}
-	r.Table(blog.PostTable).OrderBy(r.Desc("created")).Skip(page * NumberOfPostsPerPage).Limit(
-		NumberOfPostsPerPage).Run(this.rs).All(&posts)
+	tag := this.Ctx.Input.Param(":tag")
+	year := this.Ctx.Input.Param(":year")
+	if len(tag) > 0 {
+		query = query.Filter(r.Row.Attr("tags").Contains(tag))
+	}
+
+	if len(year) > 0 {
+		t, _ := time.Parse("2006-01-02T15:04:05Z", "2009-09-02T23:34:00Z")
+		fmt.Println(t)
+		query = query.Filter(r.Row.Attr("created").TypeOf().Eq("STRING"))
+	}
+
+	query.OrderBy(r.Desc("created")).Skip(
+		page * NumberOfPostsPerPage).Limit(NumberOfPostsPerPage).Run(this.rs).All(
+		&posts)
+
 	this.Data["Posts"] = posts
 }
--- main.go
+++ main.go
@@ -11,5 +11,7 @@ func main() {
 	beego.Router("/login/", &controllers.LoginController{})
 	beego.Router("/logout/", &controllers.LogoutController{})
 	beego.Router("/blog/", &controllers.PostListController{})
+	beego.Router(`/blog/tag/:tag([\w-]+)/`, &controllers.PostListController{})
+	beego.Router(`/blog/:year(\d{4})/`, &controllers.PostListController{})
 	beego.Run()
 }
